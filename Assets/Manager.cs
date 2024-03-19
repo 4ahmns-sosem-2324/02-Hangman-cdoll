24b19788b1cfd5b3f483f3fbaff13ba00be0c030
CASDoll <constanze.doll05@htl-salzburg.ac.at>
2024-03-19 21:40:21 +0100

    Manager Script hinzugefuegt wo ich die einzelnen Schritte die zu machen sind noch angefuegt habe

---
+++ Assets/Manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+   /*
+   Zuerst müssen wir Wörter festlegen die dann mit Random Range ausgewählt werden um sie zu erraten
+   Danach muss es funktionieren das man irgendeine Eingabemöglichketi hat, sei es InputField oder Buchstaben auf der Tastatur
+   Wir müssen einen count machen der die Versuche mitzählt 
+   Wir brauchen einen Überprüfer der überprüft ob der Buchstabe dne wir eingegeben haben in dem Wort vorhanden ist oder nicht 
+   Dann brauchen wir einen Restart button um es neu zu starten wenn wir es falsch haben oder ein zweites machen wollen
+   Wir brauchen einen Text der wenn es falsch ist, ausgibt was das richtige Wort gewesen wäre
+    */
+}
